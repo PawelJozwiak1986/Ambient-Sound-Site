31e3008e86d7620d618731eb490d3154dea4d236
Pawel <paweljozwiak1986@gmail.com>
2019-03-06 16:55:25 +0100

    setting up button in js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const playButton = document.querySelector('.play-pause__btn');
+const audio = new Audio('chchołowska dolina.mp3');
+
+console.log(playButton);
+ 
+let playAudio = () => {
+  console.log(playButton);
+}
