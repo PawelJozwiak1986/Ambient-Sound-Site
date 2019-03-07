6f38233a668cbd7de734bc6c6c8f2d4b30737633
Pawel <paweljozwiak1986@gmail.com>
2019-03-07 14:48:42 +0100

    volume slider works

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 const playButton = document.querySelector('.play-pause__btn');
 const audio = new Audio('./assets/audio.mp3');
 
-
+/////////// Play/Pause button /////////////
 let playAudio = () => {
-    
   if (audio.paused){
-    playButton.src ='./assets/pause-button.jpg';
+    playButton.src ='./assets/pause-button.png';
     audio.play()
   } else {
     playButton.src ='./assets/play-button.png';
     audio.pause();
   }  
+  volume__slider.addEventListener("mousemove", setvolume); 
 };
+let setvolume =() =>{
+  audio.volume = volume__slider.value / 100;
+}
+
