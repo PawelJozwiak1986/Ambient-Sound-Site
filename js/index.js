6131e99f9934a75f8a19e16bade81c6240de7922
Pawel <paweljozwiak1986@gmail.com>
2019-03-07 10:48:13 +0100

    play button fixes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
-const playButton = document.querySelector('#play-pause__btn');
+const playButton = document.querySelector('.play-pause__btn');
 const audio = new Audio('./assets/audio.mp3');
 
 let playAudio = () => {
+
+
   audio.play()
 };
