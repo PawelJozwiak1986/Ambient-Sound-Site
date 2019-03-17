ed170181a13cf1afe468e7cc41d2a89b2d92bce6
Pawel <paweljozwiak1986@gmail.com>
2019-03-17 16:06:23 +0100

    change bg imges

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const playButton = document.querySelector('.play-pause__btn');
 const audio = new Audio('./assets/audio.mp3');
-
+const bgImage = document.querySelector('body');
+console.log(bgImage)
 /////////// Play/Pause button /////////////
 let playAudio = () => {
   if (audio.paused){
@@ -16,3 +17,36 @@ let setvolume =() =>{
   audio.volume = volume__slider.value / 100;
 }
 
+window.setInterval(changeImage, 5000);
+
+
+function changeImage (){
+  
+
+};
+
+
+function randombg(){
+  var random= Math.floor(Math.random() * 6) + 0;
+  var bigSize = ["url('./assets/1.jpg')",
+                 "url('./assets/2.jpg')",
+                 "url('./assets/3.jpg')",
+                 "url('./assets/4.jpg')",
+                 "url('./assets/5.jpg')",
+                 "url('./assets/6.jpg')",
+                 "url('./assets/7.jpg')",
+                 "url('./assets/8.jpg')",
+                 "url('./assets/9.jpg')",
+                 "url('./assets/10.jpg')",
+                 "url('./assets/11.jpg')",
+                 "url('./assets/12.jpg')",
+                 "url('./assets/13.jpg')",
+                 "url('./assets/14.jpg')",
+                 "url('./assets/15.jpg')",
+                 "url('./assets/16.jpg')",                
+                 "url('./assets/17.jpg')",
+                ];
+document.body.style.backgroundImage=bigSize[random];
+}
+
+window.setInterval(randombg, 15000);
